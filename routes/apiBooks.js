6749a90093a7fe70b81a89ad944f634c5fe1c470
apei1113 <A8158059999@gmail.com>
2021-06-09 15:53:04 +0800

    implement post /Assignment, get /esit/:id

--- routes/apiBooks.js
+++ routes/apiBooks.js
@@ -31,4 +31,130 @@ router.get('/add', async function (req, res, next) {
   });
 });
   
+
+// add a new book
+router.post('/add', async function (req, res, next) {
+  // res.send('Add a new book.')
+
+  // const name = req.body.name;
+  // const author = req.body.author;
+  const ClassName = req.body.ClassName;
+  const HW_Name = req.body.HW_Name;
+  const HW_Grade = req.body.HW_Grade;
+
+  // console.log(name, author);
+  console.log(ClassName, HW_Name, HW_Grade);
+
+  const form_data = {
+    // name: name,
+    // author: author,
+    ClassName: ClassName,
+    HW_Name: HW_Name,
+    HW_Grade: HW_Grade,
+  };
+  
+  try {
+    // await db.query('INSERT INTO books SET ?', form_data);
+    // await db.query('INSERT INTO `0521`.w15 SET ?', form_data);
+    // await db.query('INSERT INTO `0521`.books_copy SET ?', form_data);
+ 
+    const response = await fetch('http://localhost:1337/Assignments', {
+        method: 'post',
+        body:    JSON.stringify(form_data),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    const data = await response.json();
+
+    res.redirect('/apiBooks');
+  } catch(err) {
+    console.log(err);
+    res.render('apiBooks/add', {
+      // name: form_data.name,
+      // author: form_data.author
+      ClassName: form_data.ClassName,
+      HW_Name: form_data.HW_Name,
+      HW_Grade: form_data.HW_Grade
+    });
+  }
+});
+
+// display edit book page
+// router.get('/edit/:id', async function (req, res, next) {
+  router.get('/edit/:id', async function (req, res, next) {
+    // res.send('display edit book page');
+    const id = req.params.id;
+    // const ClassName = req.params.ClassName;
+    try {
+      const response = await fetch(`http://localhost:1337/Assignments/${id}`);
+      const data = await response.json();
+      
+      res.render('apiBooks/edit', {
+        id: data.id,
+        // name: rows[0].name,
+        // author: rows[0].author
+        ClassName: data.ClassName,
+        HW_Name: data.HW_Name,
+        HW_Grade: data.HW_Grade
+      });
+    } catch(err) {
+      console.log(err);
+    }
+  });
+
+router.post('/update', async function (req, res, next) {
+  // res.send('update book data');
+  // const name = req.body.name;
+  // const author = req.body.author;
+  const ClassName = req.body.ClassName;
+  const HW_Name = req.body.HW_Name;
+  const HW_Grade = req.body.HW_Grade;
+  const id = req.body.id;
+
+  try {
+
+      
+
+      await db.query('UPDATE `0521`.books_copy SET ClassName = ?, HW_Name = ?, HW_Grade = ? WHERE id = ?',[
+      // name,
+      // author,
+      ClassName,
+      HW_Name,
+      HW_Grade,
+      id,
+    ]);
+    // res.status(200).json({ message: 'Updating successful' });
+    res.redirect('/books');
+  } catch(err){
+    console.log(err);
+  }
+});
+
+router.post('/update', async function (req, res, next) {
+  // res.send('update book data');
+  // const name = req.body.name;
+  // const author = req.body.author;
+  const ClassName = req.body.ClassName;
+  const HW_Name = req.body.HW_Name;
+  const HW_Grade = req.body.HW_Grade;
+  const id = req.body.id;
+
+  try {
+    // await db.query('UPDATE books SET name = ?, author = ? WHERE id = ?',[
+    // await db.query('UPDATE `0521`.w15 SET name = ?, author = ? WHERE id = ?',[
+      await db.query('UPDATE `0521`.books_copy SET ClassName = ?, HW_Name = ?, HW_Grade = ? WHERE id = ?',[
+      // name,
+      // author,
+      ClassName,
+      HW_Name,
+      HW_Grade,
+      id,
+    ]);
+    // res.status(200).json({ message: 'Updating successful' });
+    res.redirect('/apiBooks');
+  } catch(err){
+    console.log(err);
+  }
+});
+
 module.exports = router;
